74c976ec8cfb672adae36cb6de4a4504b04b2ac1
justinvzyl <justin.vanzyl@gmail.com>
2019-05-15 13:39:28 +1200

    Added 'UserCommentList' and 'UserCommentDetail' API endpoints

--- txtnlpapi/api/views.py
+++ txtnlpapi/api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from .models import UserComment
+from .serializers import UserCommentSerializer
+from rest_framework import generics
 
-# Create your views here.
+class UserCommentList(generics.CreateAPIView):
+    queryset = UserComment.objects.all()
+    serializer_class = UserCommentSerializer
+
+class UserCommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = UserComment.objects.all()
+    serializer_class = UserCommentSerializer
