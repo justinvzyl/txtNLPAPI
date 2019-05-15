59fb01cc99dce08c9910756283b6031fee48f26c
justinvzyl <justin.vanzyl@gmail.com>
2019-05-15 13:28:22 +1200

    Added initial 'UserComment' model

--- txtnlpapi/api/models.py
+++ txtnlpapi/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class UserComment(models.Model):
+    created = models.DateTimeField(auto_add_now = True)
+    #user
+    topic = models.CharField(max_length = 254, blank = False, required = True, default = 'None')
+    comment = models.TextField(required = True, blank  = False)
+    polarity = models.FloatField(blank = True, default = 0)
+    subjectivity = models.FloatField(blank = True, default = 0)
+    
+    class Meta:
+        ordering = ('created',)
+
+
+
